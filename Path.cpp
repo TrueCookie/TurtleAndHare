371445dce7f9f9d0427a7beaad5af2948305da0f
TrueCookie <artemogloblin56@gmail.com>
2019-03-29 00:54:21 +0300

    vector tracks

--- Path.cpp
+++ Path.cpp
@@ -3,44 +3,58 @@
 #include <string>
 
 Path::Path(int num){
-	track = new char[70];
+	tracksNum = 0;
+	//tracks = new char*[tracksNum];
 	racersNum = num;
 	racersPos = new int[num];
 }
 
 Path::~Path(){
-	delete[] track;
+	//delete[] tracks;
 }
 
 void Path::print() {
 	for (int i = 0; i < 70; ++i) {
-		std::cout << track[i];
+		std::cout << tracks.back[i];
 	}
 }
 
 void Path::writeOuch(int pos) {
 	std::string ouch("OUCH!!!");
 	for (int i = pos; i < pos + 7; ++i) {
-		track[i] = ouch[i-pos];
+		tracks.back[i] = ouch[i-pos];
 	}
 }
 
-bool Path::update(Animal** racers) {
+bool Path::move(Animal** racers) {
 	for (int i = 0; i < racersNum; ++i) {//move racers
 		racersPos[i] += racers[i]->defMove();
 		if (racersPos[i] < 0) {	//check out of range case
 			racersPos[i] = 0;
 		}
 		else if (racersPos[i] > 69) {
+			printEnd(racers[racersPos[i]]->getName);
 			return true;
 		}
 	}
-	track = new char[70];
+	return false;
+}
+
+
+void Path::printEnd(char name) {
+	std::cout << name << " won" << std::endl;
+}
+
+bool Path::update(Animal** racers) {
+	move(racers);
+	char* curTrack = new char[70];
+	tracks.push_back(curTrack);
+	memset(curTrack, ' ', 70);
 	//checking collusion for everyone
 	for (int i = 0; i < racersNum-1; ++i){
 		for (int j = i+1; j < racersNum; ++j) {
 			if (racersPos[i] != racersPos[j]) {
-				track[racersPos[i]] = racers[i]->getName();
+				curTrack[racersPos[i]] = racers[i]->getName();
 			}
 			else {
 				writeOuch(racersPos[i]);
